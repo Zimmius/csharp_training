bcc92b5eb1a9a5e650e25708a26731840776bc57
Zimmius <grybakov@usetech.ru>
2022-02-22 22:57:07 +0300

    Добавлен базовый класс для спомогательных методов. Переписаны тесты с использованием методов из вспомогательного класса.

--- addressbook-web-tests/addressbook-web-tests/GroupRemovalTests.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupRemovalTests.cs
@@ -7,20 +7,16 @@ using NUnit.Framework;
 namespace WebAddressbookTests
 {
     [TestFixture]
-    public class GruopCreationTests : TestBase
+    public class GroupRemovalTests : TestBase
     {
         [Test]
-        public void GroupCreationTest()
+        public void GroupRemovalTest()
         {
             GoToHomePage();
             Login(new AccountData("admin","secret"));
             GoToGroupsPage();
-            InitGroupCreation();
-            GroupData group = new GroupData("aaa");
-            group.Header = "bbb";
-            group.Footer = "ccc";
-            FillGroupForm(group);
-            SubmitGroupCreation();
+            SelectGroup(1);
+            DeleteGroup();
             ReturnToGroupPage();
         }
     }
